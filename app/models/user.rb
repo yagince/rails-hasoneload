9c993211c92935f2f31828ab3f81c9c69ee8ae90
yagince <straitwalk@gmail.com>
2021-08-31 23:28:44 +0900

    impl not exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   has_many :posts
 
-  has_one :latest_post, -> { order(posted_at: :desc) }, class_name: :Post
+  has_one :latest_post, -> {
+    where(
+      <<~SQL
+        NOT EXISTS (
+          SELECT 1 FROM posts AS p
+            WHERE
+              posts.posted_at < p.posted_at AND
+              posts.user_id = p.user_id
+        )
+      SQL
+    )
+  }, class_name: :Post
 end
